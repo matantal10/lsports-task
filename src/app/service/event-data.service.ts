0ccef4faa04a3c08b74411acd64c50d568067425
matant1 <matant@mprest.com>
2022-02-10 00:27:45 +0200

    add expand, format date display

--- src/app/service/event-data.service.ts
+++ src/app/service/event-data.service.ts
@@ -15,7 +15,7 @@ export class EventDataService {
       location: 'New-York',
       league: 'NBA',
       teamsPlaying: 'Knicks VS Lakers',
-      startingTime: new Date(''),
+      startingTime: new Date(Date.now()),
       additionalData: ' LeBron James returned from a five-game absence due to injury and had a triple-double of 29 points, 13 rebounds, 10 assists, and the Los Angeles Lakers rallied from a 21-point, second-quarter deficit, beating the New York Knicks 122-115 in overtime Saturday night.',
     },
       {
@@ -24,7 +24,7 @@ export class EventDataService {
         location: 'New-York',
         league: 'NBA',
         teamsPlaying: 'Knicks VS Lakers',
-        startingTime: new Date(''),
+        startingTime: new Date(Date.now()),
         additionalData: ' LeBron James returned from a five-game absence due to injury and had a triple-double of 29 points, 13 rebounds, 10 assists, and the Los Angeles Lakers rallied from a 21-point, second-quarter deficit, beating the New York Knicks 122-115 in overtime Saturday night.',
       },
       {
@@ -33,7 +33,7 @@ export class EventDataService {
         location: 'New-York',
         league: 'NBA',
         teamsPlaying: 'Knicks VS Lakers',
-        startingTime: new Date(''),
+        startingTime: new Date(Date.now()),
         additionalData: ' LeBron James returned from a five-game absence due to injury and had a triple-double of 29 points, 13 rebounds, 10 assists, and the Los Angeles Lakers rallied from a 21-point, second-quarter deficit, beating the New York Knicks 122-115 in overtime Saturday night.',
       },
       {
@@ -42,7 +42,7 @@ export class EventDataService {
         location: 'New-York',
         league: 'NBA',
         teamsPlaying: 'Knicks VS Lakers',
-        startingTime: new Date(''),
+        startingTime: new Date(Date.now()),
         additionalData: ' LeBron James returned from a five-game absence due to injury and had a triple-double of 29 points, 13 rebounds, 10 assists, and the Los Angeles Lakers rallied from a 21-point, second-quarter deficit, beating the New York Knicks 122-115 in overtime Saturday night.',
       },
       {
@@ -51,7 +51,7 @@ export class EventDataService {
         location: 'New-York',
         league: 'NBA',
         teamsPlaying: 'Knicks VS Lakers',
-        startingTime: new Date(''),
+        startingTime: new Date(Date.now()),
         additionalData: ' LeBron James returned from a five-game absence due to injury and had a triple-double of 29 points, 13 rebounds, 10 assists, and the Los Angeles Lakers rallied from a 21-point, second-quarter deficit, beating the New York Knicks 122-115 in overtime Saturday night.',
       },
 
@@ -65,7 +65,7 @@ export class EventDataService {
       league: "",
       location: "",
       sportName: "",
-      startingTime: new Date(),
+      startingTime: new Date(Date.now()),
       teamsPlaying: ""
     };
 
--- src/app/sport-event-table/sport-event-table.component.ts
+++ src/app/sport-event-table/sport-event-table.component.ts
@@ -16,19 +16,25 @@ export class SportEventTableComponent implements OnInit {
   tableHeaders: ICol[] = ["sportName","location", "league", "teamsPlaying", "startingTime"];
   isEdit = false;
   currentRow: any = null;
+  nextId: number;
+  isExpand = false;
+  selectedRow: IEvent = null;
 
   constructor(private eventData: EventDataService) { }
 
   ngOnInit(): void {
     this.eventTableList = this.eventData.getAllEvents();
+    this.maxId();
+
   }
 
   getAdditionalDetails(event: IEvent) {
-
+    this.selectedRow = event;
+    this.isExpand = !this.isExpand;
   }
 
   editEvent(event: IEvent) {
-    this.isEdit = true
+    this.isEdit = true;
     this.currentRow = event;
   }
 
@@ -38,20 +44,28 @@ export class SportEventTableComponent implements OnInit {
   }
 
   addNewEvent() {
-    this.isEdit = true;
-    this.currentRow = this.eventData.createNewEvent();
-    this.currentRow.id = this.maxId();
-    this.eventTableList.push(this.currentRow);
+    if(!this.isEdit) {
+      this.isEdit = true;
+      this.currentRow = this.eventData.createNewEvent();
+      this.currentRow.id = this.nextId;
+      this.eventTableList.push(this.currentRow);
+      this.maxId();
+    }
+
   }
 
   deleteEvent(event: IEvent) {
     const index = this.eventTableList.findIndex(entity => entity.id === event.id);
     this.eventTableList.splice(index, 1);
+    this.isEdit = false;
   }
 
-  private maxId(): number {
-    let maxid: number = 1;
-
-    return maxid;
+  private maxId(): void {
+    if(this.eventTableList.length === 0) {
+      this.nextId = 0;
+    } else {
+      let maxId = Number(this.eventTableList[this.eventTableList.length - 1].id);
+      this.nextId = maxId + 1;
+    }
   }
 }
